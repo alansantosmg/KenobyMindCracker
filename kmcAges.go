0c9b3e8c6b2cb43c97ae2755fc25e52ccf90e5a1
Alan Santos <alansantosmg@gmail.com>
2020-03-16 16:01:31 -0300

    kmcAges documentado

--- kmcAges.go
+++ kmcAges.go
@@ -7,14 +7,19 @@ import (
 	"strconv"
 )
 
+//stringToFloat converts string to float64 number
 func stringToFloat(s string) (float64, error) {
 	f, err := strconv.ParseFloat(s, 64)
+
+	// if cannot convert return error message
 	if err != nil {
 		err = errors.New("Argumento não é numero")
 	}
 	return f, err
 }
 
+// calculaResultado gets  age averages and people groups
+// then calc the difference of the 2 groups avarages
 func calculaResultado(mediaIdades1, quantidadePessoas1, mediaIdades2, quantidadePessoas2 float64) float64 {
 	mediaIdades1 *= quantidadePessoas1
 	mediaIdades2 *= quantidadePessoas2
@@ -27,7 +32,10 @@ func calculaResultado(mediaIdades1, quantidadePessoas1, mediaIdades2, quantidade
 
 func main() {
 
+	// check if user provided the correct number of arguments
 	if len(os.Args) != 5 {
+
+		// print help
 		fmt.Printf(`
 
 KmcAges - Resolve problema sobre encontrar a idade de uma pessoa usando a diferença de médias de idade entre 2 grupos.
@@ -43,8 +51,10 @@ Execute: kmcAges 45 2 40 3
 
 	} else {
 
+		// storage strings converted to floats
 		commandArgs := make([]float64, 4)
 
+		//pass os.Args[] to convert values to float
 		for i, arg := range os.Args[1:] {
 			c, err := stringToFloat(arg)
 			commandArgs[i] = c
@@ -53,6 +63,7 @@ Execute: kmcAges 45 2 40 3
 			}
 		}
 
+		// Return result to user
 		resultado := calculaResultado(commandArgs[0], commandArgs[1], commandArgs[2], commandArgs[3])
 		fmt.Println("x = ", resultado)
 
